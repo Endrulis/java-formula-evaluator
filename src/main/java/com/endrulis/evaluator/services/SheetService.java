cde30490cbb3e505dcfff4c51ab8cf86b8b463aa
Endrulis <endrulisr@gmail.com>
2023-04-10 21:37:20 +0300

    Extracted methods from evaluateSheet method

--- src/main/java/com/endrulis/evaluator/services/SheetService.java
+++ src/main/java/com/endrulis/evaluator/services/SheetService.java
@@ -4,6 +4,10 @@ import com.endrulis.evaluator.entities.RequestBody;
 import com.endrulis.evaluator.entities.MySheet;
 import com.endrulis.evaluator.entities.SpreadSheet;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -11,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.Console;
 import java.util.*;
 
 import static com.endrulis.evaluator.constants.AppConstants.*;
@@ -51,6 +56,7 @@ public class SheetService {
         List<MySheet> updatedSheets = new ArrayList<>();
         for(MySheet sheet : spreadSheet.getSheets()){
             MySheet updatedSheet = new MySheet(sheet.getId(), evaluateSheet(sheet.getData()));
+            System.out.println(sheet.getData());
             updatedSheets.add(updatedSheet);
         }
         RequestBody requestBody = new RequestBody(CONST_EMAIL, spreadSheet.getSheets());
@@ -61,15 +67,40 @@ public class SheetService {
     }
 
     private List<List<Object>> evaluateSheet(List<List<Object>> data) {
-        Workbook workbook = new XSSFWorkbook();
-        Sheet sheet = workbook.createSheet();
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet();
         for(int i = 0; i < data.size(); i++) {
-            Row row = sheet.createRow(i);
+            XSSFRow row = sheet.createRow(i);
             for(int j = 0; j < data.get(i).size(); j++) {
-                Cell cell = row.createCell(j);
+                XSSFCell cell = row.createCell(j);
                 if(data.get(i).get(j) instanceof String && ((String) data.get(i).get(j)).startsWith("=")) {
-                    cell.setCellFormula(((String) data.get(i).get(j)).substring(1));
-                } else {
+                    String formula = ((String) data.get(i).get(j)).substring(1);
+                    System.out.println(formula);
+                    if (formula.startsWith("MULTIPLY(")) {
+                        cell.setCellFormula(evaluateMultiply(formula));
+                    } else if (formula.startsWith("DIVIDE(")) {
+                        cell.setCellFormula(evaluateDivision(formula));
+                    } else if (formula.startsWith("GT(")) {
+                        cell.setCellFormula(evaluateGT(formula));
+                    } else if (formula.startsWith("EQ(")) {
+                        cell.setCellFormula(evaluateEQ(formula));
+                    } else if (formula.startsWith("NOT(")) {
+                        cell.setCellFormula(evaluateNOT(formula));
+                    }else if (formula.startsWith("IF(")) {
+                        String[] args = formula.substring(3, formula.length() - 1).split(",");
+                        String condition = args[0].trim();
+                        String trueValue = args[1].trim();
+                        String falseValue = args[2].trim();
+                        StringBuilder sb = new StringBuilder();
+                        sb.append("IF(").append(condition).append("))*").append(falseValue);
+                        cell.setCellFormula(sb.toString());
+                    }else {
+                        cell.setCellFormula(formula);
+                    }
+                }else if(data.get(i).get(j) instanceof Boolean){
+                    cell.setCellValue(Boolean.parseBoolean(data.get(i).get(j).toString()));
+                }
+                else {
                     cell.setCellValue(Double.parseDouble(data.get(i).get(j).toString()));
                 }
             }
@@ -93,5 +124,54 @@ public class SheetService {
             result.add(rowValues);
         }
         return result;
+    }
+
+    private static String evaluateNOT( String formula ) {
+        String arg = formula.substring(4, formula.length() - 1).trim();
+        StringBuilder sb = new StringBuilder();
+        sb.append("(NOT(").append(arg).append("))*1");
+        return sb.toString();
+    }
+
+    private static String evaluateEQ( String formula ) {
+        String[] args = formula.substring(3, formula.length() - 1).split(",");
+        String arg1 = args[0].trim();
+        String arg2 = args[1].trim();
+        StringBuilder sb = new StringBuilder();
+        sb.append("(").append(arg1).append("=").append(arg2).append(")*1");
+        return sb.toString();
+    }
+
+    private static String evaluateGT( String formula ) {
+        String[] args = formula.substring(3, formula.length() - 1).split(",");
+        String arg1 = args[0].trim();
+        String arg2 = args[1].trim();
+        StringBuilder sb = new StringBuilder();
+        sb.append("(").append(arg1).append(">").append(arg2).append(")*1");
+        return sb.toString();
+    }
+
+    private String evaluateDivision( String formula ) {
+        String[] args = formula.substring(7, formula.length() - 1).split(",");
+        StringBuilder sb = new StringBuilder();
+        for (int k = 0; k < args.length; k++) {
+            if (k > 0) {
+                sb.append("/");
+            }
+            sb.append(args[k].trim());
+        }
+        return sb.toString();
+    }
+
+    private String evaluateMultiply( String formula ) {
+        String[] args = formula.substring(9, formula.length() - 1).split(",");
+        StringBuilder sb = new StringBuilder();
+        for (int k = 0; k < args.length; k++) {
+            if (k > 0) {
+                sb.append("*");
+            }
+            sb.append(args[k].trim());
+        }
+        return sb.toString();
     }
 }
